435857e49cf4067ab31a9a4ffd914e042f767377
SkySarDev <skysar@ya.ru>
2022-01-16 22:29:28 +0300

    [chore] rename store to reduxStore

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 
-import { store } from "store";
+import { store } from "store/reduxStore";
 
 import App from "./App";
 
